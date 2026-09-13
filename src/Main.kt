93ad0f4df58be5498104ea034ef7e3069fa95635
kf8377667-del <kf8377667@gmail.com>
2026-09-13 16:02:05 +0300

    Четвертое задание выполнено

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+    println("Введите два числа и знак операции через пробел")
+    val line = readLine()!!
+    val parts = line.split(" ")
+    val num1 = parts[0].toDouble()
+    val num2 = parts[1].toDouble()
+    val op = parts[2][0]
+    val result = when (op)
+    {
+        '+' -> num1 + num2
+        '-' -> num1 - num2
+        '*' -> num1 * num2
+        '/' -> num1 / num2
+        else -> 0.0
+    }
+    println("Результат: $result")
+}
