a3fb1a7767cefe9027af32c461a36d66d59161c7
InterLove <kf8377667@gmail.com>
2026-09-24 22:41:27 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -3,11 +3,14 @@ fun main()
     println("Введите два числа и знак операции через пробел")
     val line = readLine()!!
     val parts = line.split(" ")
+    if (parts.size != 3){
+        println("Ошибка: Не удалось распознать числа")
+        return
+    }
     val num1 = parts[0].toDouble()
     val num2 = parts[2].toDouble()
     val op = parts[1][0]
-    val result = when (op)
-    {
+    val result = when (op){
         '+' -> num1 + num2
         '-' -> num1 - num2
         '*' -> num1 * num2
